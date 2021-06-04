07e698eb50f22f2e1b21354239f2c95f14d79512
Tarush Goyal <tarush@Tarushs-MacBook-Air.local>
2021-06-05 04:04:21 +0530

    fixed bugs

--- index.js
+++ index.js
@@ -5,65 +5,32 @@ const io = require("socket.io")(http);
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-//Using CORS policy
-
-// const corsOptions = {
-//   origin: "http://localhost:3000",
-//   credentials: true,
-//   optionSuccessStatus: 200,
-// };
-// app.use(cors(corsOptions));
-
-// const bodyParser = require("body-parser");
-// var jsonParser = bodyParser.json();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-
-  next();
-});
-
-// Using Node.js `require()`FOR mongoDB
-
-// configure mongoDB
 const mongoDB = `mongodb+srv://SahyogAdmin:${process.env.DB_PASSWORD}@sahyog.upfyh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-//connect local database
+
 mongoose
   .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Database Connected..."))
   .catch((err) => console.log("Error connecting database", err));
 
-//To remove depreceate warning
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 
 const Room = require("./models/Room");
 
-//post request to join room
 app.post("/join_room", async (req, res) => {
-  // console.log('req reciveed', req.body);
-
-  //check if the room which this exist or not
   const room_id = req.body.room_id;
   const oyo_room = await Room.findOne({ uID: room_id }).catch((err) => {
     console.log("error occured while checking room", err);
   });
 
-  // console.log('room' ,oyo_room);
   flag = false;
   if (oyo_room) {
-    // check if room has less than 2 user
     if (oyo_room.noOfUser < 2) {
-      // increase no of user
       oyo_room.noOfUser++;
       const doc = await oyo_room.save();
 
@@ -78,7 +45,6 @@ app.post("/join_room", async (req, res) => {
 });
 
 app.get("/create_room", (req, res) => {
-  //Generating unique id for each room
   var alphabet = [
     "a",
     "b",
@@ -113,7 +79,6 @@ app.get("/create_room", (req, res) => {
     result += alphabet[Math.floor(Math.random() * 10000) % 25];
   }
 
-  //Saving newly creted roomt to database
   const room = new Room({ uID: result, noOfUser: 1 });
   room
     .save()
@@ -127,14 +92,11 @@ app.get("/create_room", (req, res) => {
   res.json(result);
 });
 
-//Sample Request
 app.get("/", (req, res) => {
   res.send({ message: "We did it!" });
 });
 
-//Open Socket io Connection
 io.on("connection", (socket) => {
-  // user join register room_id
   socket.on("join", async (room_id) => {
     console.log("user joined", room_id);
     socket.join(room_id);
@@ -147,7 +109,7 @@ io.on("connection", (socket) => {
       io.to(room_id).emit("youCanPLayNow");
     }
   });
-  //incoming message from chat.js
+
   socket.on("sendMessage", async ({ message, name, user_id, room_id }) => {
     const msgToStore = {
       name,
@@ -175,7 +137,6 @@ io.on("connection", (socket) => {
   });
 });
 
-//Start Up Server
 const PORT = process.env.PORT || 8000;
 http.listen(PORT, () => {
   console.log("Backend Server listing at PORT:", PORT);
